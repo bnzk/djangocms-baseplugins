9479be9e6294b6b2bbd277689e92884a49b154e4
benzkji <bnzk@bnzk.ch>
2017-11-02 21:26:42 +0100

    enhancements when overriding render()

--- djangocms_baseplugins/baseplugin/cms_plugins.py
+++ djangocms_baseplugins/baseplugin/cms_plugins.py
@@ -3,10 +3,12 @@ from __future__ import unicode_literals
 
 class BasePluginMixin():
 
-    def render(self, context, instance, placeholder):
+    def get_render_template(self, context, instance, placeholder):
         request = context.get('request', None)
         if not instance.published and not request.toolbar.edit_mode:
             self.render_template = 'djangocms_baseplugins/unpublished.html'
+
+    def render(self, context, instance, placeholder):
         context['plugin'] = self
         context['object'] = instance
         context['placeholder'] = placeholder
