5c7f120293c0660f79da2ecfd115f32ad2a2a83a
benzkji <bnzk@bnzk.ch>
2018-05-13 10:18:47 +0200

    more base tests

--- djangocms_baseplugins/baseplugin/tests/base.py
+++ djangocms_baseplugins/baseplugin/tests/base.py
@@ -10,6 +10,14 @@ class BasePluginTestCase(object):
     plugin_class = None  # TextPlugin
     plugin_settings_prefix = ''  # TEXTPLUGIN
 
+    def test_basic_admin_form(self):
+        """
+        just calling add plugin admin view
+        like /admin/cms/page/add-plugin/?placeholder_id=43&plugin_type=SectionPlugin&cms_path=%2Fen%2F&plugin_language=en
+        :return:
+        """
+        return
+
     def test_plugin_context(self):
         placeholder = Placeholder.objects.create(slot='test')
         model_instance = add_plugin(
@@ -40,14 +48,11 @@ class BasePluginTestCase(object):
         renderer = ContentRenderer(request=RequestFactory())
         html = renderer.render_plugin(model_instance, {})
         plugin_name = model_instance.__class__.__name__.lower()
-        print(html)
-        self.assertInHTML('plugin-{}'.format(plugin_name), force_text(html))
-        self.assertInHTML('plugin-{}_{}'.format(plugin_name, 'layout-value'), force_text(html))
-        self.assertInHTML('plugin-{}_{}'.format(plugin_name, 'color-value'), force_text(html))
-        self.assertInHTML('plugin-{}_{}'.format(plugin_name, 'background-value'), force_text(html))
-
-
-
+        self.assertIn('plugin_{}'.format(model_instance.pk), force_text(html))
+        self.assertIn('plugin-{}'.format(plugin_name), force_text(html))
+        self.assertIn('plugin-{}_{}'.format(plugin_name, 'layout-value'), force_text(html))
+        self.assertIn('plugin-{}_{}'.format(plugin_name, 'color-value'), force_text(html))
+        self.assertIn('plugin-{}_{}'.format(plugin_name, 'background-value'), force_text(html))
 
     def test_form_choices_and_other_settings_respected(self):
         """
