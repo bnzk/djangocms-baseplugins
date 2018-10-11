b0eb9f61634f948a71cc30cfdafe97eed7a9c14a
benzkji <bnzk@bnzk.ch>
2018-10-11 09:54:47 +0200

    new fields

--- djangocms_baseplugins/contentnav/models.py
+++ djangocms_baseplugins/contentnav/models.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
-from cms.models import CMSPlugin
+from cms.models.fields import PageField
 from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
@@ -11,6 +11,19 @@ from djangocms_baseplugins.baseplugin.models import AbstractBasePlugin
 
 @python_2_unicode_compatible
 class ContentNavBase(AbstractBasePlugin):
+
+    menu_depth = models.PositiveIntegerField(
+        default=1,
+        verbose_name=_("Depth")
+    )
+    cms_page = PageField(
+        default=None,
+        help_text=_("Show submenu of this page")
+    )
+    sitemap = models.BooleanField(
+        default=False,
+        help_text=_("Show complete sitemap")
+    )
 
     class Meta:
         abstract = True
--- djangocms_baseplugins/contentnav/tests/test_contentnavplugin.py
+++ djangocms_baseplugins/contentnav/tests/test_contentnavplugin.py
@@ -1,10 +1,6 @@
 from __future__ import unicode_literals
 
-from cms.api import add_plugin
-from cms.models import Placeholder
-from cms.plugin_rendering import ContentRenderer
 from django.test import TestCase, RequestFactory
-from django.utils.encoding import force_text
 
 from djangocms_baseplugins.baseplugin.tests.base import BasePluginTestCase
 from djangocms_baseplugins.contentnav.cms_plugins import ContentNavPlugin
