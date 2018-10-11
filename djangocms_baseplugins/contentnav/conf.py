5508aa27caba35b74a33c9518eca4c2133f67b80
benzkji <bnzk@bnzk.ch>
2018-10-11 11:11:43 +0200

    fix import

--- djangocms_baseplugins/contentnav/conf.py
+++ djangocms_baseplugins/contentnav/conf.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
 from djangocms_baseplugins.baseplugin import defaults
 from djangocms_baseplugins.baseplugin.utils import build_baseplugin_fieldset
