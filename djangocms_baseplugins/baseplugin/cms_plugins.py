4a005451cdde7a9ceceb45181bb7d7bde08eb2f8
Ben Stähli <bnzk@bnzk.ch>
2020-09-02 09:26:01 +0200

    ParentPluginFormMixin

--- djangocms_baseplugins/baseplugin/cms_plugins.py
+++ djangocms_baseplugins/baseplugin/cms_plugins.py
@@ -1,26 +1,54 @@
 from __future__ import unicode_literals
 
+from django import forms
+from cms.models import CMSPlugin
 from modeltranslation.admin import TranslationAdmin, TranslationInlineModelAdmin
 
 from djangocms_baseplugins.baseplugin.utils import is_edit_mode
 from . import defaults
 
+
 if defaults.TRANSLATE:
     from djangocms_misc.basic.admin import LanguageTabsMixin
+
 
     class BasePluginMixinBase(LanguageTabsMixin, TranslationAdmin):
         pass
 
+
     class BaseInlineMixinBase(TranslationInlineModelAdmin):
         pass
 
+
 else:
+
 
     class BasePluginMixinBase(object):
         pass
 
+
     class BaseInlineMixinBase(object):
         pass
+
+
+class ParentPluginFormMixin(object):
+    """
+    mostly used when fine grained control of child plugins forms is needed
+    - hide/show fields, based on parent plugin type/field values
+    - different choices, based on parent plugin type/field values
+    """
+    def __init__(self, *args, **kwargs):
+        super_result = super().__init__(*args, **kwargs)
+        parent = None
+        if kwargs.get('initial', None):
+            if kwargs['initial'].get('plugin_parent', None):
+                parent = CMSPlugin.objects.get(pk=kwargs['initial']['plugin_parent'])
+        elif self.instance.parent:
+            parent = self.instance.parent
+        self.parent_plugin = parent
+        if parent:
+            self.parent_plugin_instance, plugin = parent.get_plugin_instance()
+        return super_result
 
 
 class BasePluginMixin(BasePluginMixinBase):
