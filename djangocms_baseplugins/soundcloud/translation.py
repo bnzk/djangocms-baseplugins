ee65cdff0306334bd70b66aab95253b2b78aa9f8
Ben Stähli <bnzk@bnzk.ch>
2021-08-18 18:02:40 +0200

    working sc plugin, translation fix

--- djangocms_baseplugins/soundcloud/translation.py
+++ djangocms_baseplugins/soundcloud/translation.py
@@ -3,12 +3,12 @@ from modeltranslation.translator import TranslationOptions, translator
 
 from djangocms_baseplugins.baseplugin import defaults
 from . import conf
-from .models import SoundCloud
+from .models import Soundcloud
 
 
-class SoundCloudPluginTranslationOptions(TranslationOptions):
+class SoundcloudPluginTranslationOptions(TranslationOptions):
     fields = defaults.TRANSLATED_FIELDS + conf.TRANSLATED_FIELDS
 
 
 if getattr(settings, 'DJANGOCMS_BASEPLUGINS_TRANSLATE', None):
-    translator.register(SoundCloud, SoundCloudPluginTranslationOptions)
+    translator.register(Soundcloud, SoundcloudPluginTranslationOptions)
