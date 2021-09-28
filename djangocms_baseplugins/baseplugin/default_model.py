c11c6d3a5400d06bd6a64c48d9b4766a3e314a66
Ben Stähli <bnzk@bnzk.ch>
2021-09-28 16:04:22 +0200

    another settings cleanup/failsafe code

--- djangocms_baseplugins/baseplugin/default_model.py
+++ djangocms_baseplugins/baseplugin/default_model.py
@@ -99,7 +99,7 @@ class DefaultAbstractBasePlugin(CMSPlugin):
         return False
 
     def attrs_to_string(self, text, conf):
-        if not conf.TO_STRING_ADD_ATTRS:
+        if not getattr(conf, 'TO_STRING_ADD_ATTRS', None):
             return text
         for field in defaults.ATTR_FIELDS:
             if getattr(self, field, None):
